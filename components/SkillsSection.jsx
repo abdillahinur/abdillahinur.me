98603b909c278b54c2119b81b2ccbf7f0c8021ab
Abdillahi Nur <abdillahi.mnur@gmail.com>
2024-10-13 16:22:23 -0400

    Updated skills section color

--- components/SkillsSection.jsx
+++ components/SkillsSection.jsx
@@ -1,5 +1,3 @@
-// components/SkillsSection.jsx
-
 'use client';
 
 import React from 'react';
@@ -14,28 +12,22 @@ export default function SkillsSection() {
         <Tabs.List className="flex border-b border-gray-200 dark:border-gray-700 space-x-4">
           <Tabs.Trigger
             value="languages"
-            className="px-4 py-2 -mb-px border-b-2 font-medium text-sm focus:outline-none border-transparent text-gray-600
-            dark:text-gray-400 hover:text-emerald-600 dark:hover:text-emerald-400 data-[state=active]:
-            border-emerald-500 data-[state=active]:text-emerald-600 dark:data-[state=active]:
-            text-emerald-400"
+            className="px-4 py-2 -mb-px border-b-2 font-bold text-sm focus:outline-none border-transparent text-gray-300
+            hover:text-blue-300 data-[state=active]:border-blue-400 data-[state=active]:text-blue-300"
           >
             Languages
           </Tabs.Trigger>
           <Tabs.Trigger
             value="frameworks"
-            className="px-4 py-2 -mb-px border-b-2 font-medium text-sm focus:outline-none border-transparent text-gray-600
-            dark:text-gray-400 hover:text-emerald-600 dark:hover:text-emerald-400 data-[state=active]:
-            border-emerald-500 data-[state=active]:text-emerald-600 dark:data-[state=active]:
-            text-emerald-400"
+            className="px-4 py-2 -mb-px border-b-2 font-bold text-sm focus:outline-none border-transparent text-gray-300
+            hover:text-blue-300 data-[state=active]:border-blue-400 data-[state=active]:text-blue-300"
           >
             Frameworks & Libraries
           </Tabs.Trigger>
           <Tabs.Trigger
             value="tools"
-            className="px-4 py-2 -mb-px border-b-2 font-medium text-sm focus:outline-none border-transparent text-gray-600
-            dark:text-gray-400 hover:text-emerald-600 dark:hover:text-emerald-400 data-[state=active]:
-            border-emerald-500 data-[state=active]:text-emerald-600 dark:data-[state=active]:
-            text-emerald-400"
+            className="px-4 py-2 -mb-px border-b-2 font-bold text-sm focus:outline-none border-transparent text-gray-300
+            hover:text-blue-300 data-[state=active]:border-blue-400 data-[state=active]:text-blue-300"
           >
             Tools
           </Tabs.Trigger>
@@ -43,9 +35,9 @@ export default function SkillsSection() {
 
         {/* Languages Tab */}
         <Tabs.Content value="languages" className="mt-4">
-          <Card>
+          <Card className="bg-night-sky-start bg-opacity-60">
             <CardContent className="pt-6">
-              <ul className="list-disc pl-6 space-y-2">
+              <ul className="list-disc pl-6 space-y-2 text-gray-200">
                 <li>Python</li>
                 <li>Java</li>
                 <li>JavaScript</li>
@@ -60,9 +52,9 @@ export default function SkillsSection() {
 
         {/* Frameworks & Libraries Tab */}
         <Tabs.Content value="frameworks" className="mt-4">
-          <Card>
+          <Card className="bg-night-sky-start bg-opacity-60">
             <CardContent className="pt-6">
-              <ul className="list-disc pl-6 space-y-2">
+              <ul className="list-disc pl-6 space-y-2 text-gray-200">
                 <li>React</li>
                 <li>Next.js</li>
                 <li>Angular</li>
@@ -81,9 +73,9 @@ export default function SkillsSection() {
 
         {/* Tools Tab */}
         <Tabs.Content value="tools" className="mt-4">
-          <Card>
+          <Card className="bg-night-sky-start bg-opacity-60">
             <CardContent className="pt-6">
-              <ul className="list-disc pl-6 space-y-2">
+              <ul className="list-disc pl-6 space-y-2 text-gray-200">
                 <li>Git & GitHub</li>
                 <li>Docker</li>
                 <li>Jupyter Notebook</li>
